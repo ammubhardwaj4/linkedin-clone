060bb5edf4cf23cbe946ac813359b364b9fae82d
ammubhardwaj4 <ammubhardwaj4@gmail.com>
2022-01-12 10:18:09 +0530

    linkedin-clone init file of clone added..

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const Main = () => {
+  return (
+    <div>
+      <Container>main</Container>
+    </div>
+  );
+};
+
+const Container = styled.div`
+  grid-area: Main;
+`;
+export default Main;
---
+++ src/components/Rightside.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const Rightside = () => {
+  return (
+    <div>
+      <Container>rightside</Container>
+    </div>
+  );
+};
+
+const Container = styled.div`
+  grid-area: Rightside;
+`;
+export default Rightside;
